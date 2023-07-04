800febd8382261cbfb87d62d9c843f88a91ca33a
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-03 20:06:40 -0700

    following ionic svelte tut

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,9 +1,10 @@
-import adapter from '@sveltejs/adapter-static'
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: [preprocess(), vitePreprocess({})],
 
 	kit: {
 		adapter: adapter({
