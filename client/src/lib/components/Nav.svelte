9fd7a1c00fdc50126365c374d44ec48c4fb9cec6
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-05 03:31:53 -0700

    added nav (not showing up)

---
+++ client/src/lib/components/Nav.svelte
@@ -0,0 +1,26 @@
+<ion-tabs>
+    <ion-tab-bar slot="bottom">
+        <ion-tab>
+
+            <ion-tab-button tab="home">
+                <ion-icon :icon="home-outline"></ion-icon>
+            </ion-tab-button>
+
+            <ion-tab-button tab="library">
+                <ion-icon :icon="library-outline"></ion-icon>
+            </ion-tab-button>
+            
+            <ion-tab-button tab="publish">
+                <ion-icon :icon="pencil-outline"></ion-icon>
+            </ion-tab-button>
+
+            <ion-tab-button tab="search">
+                <ion-icon :icon="search-outline"></ion-icon>
+            </ion-tab-button>
+
+            <ion-tab-button tab="user">
+                <ion-icon :icon="person-outline"></ion-icon>
+            </ion-tab-button>
+        </ion-tab>
+    </ion-tab-bar>
+</ion-tabs>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,46 +1,26 @@
-
-  <ion-card>
-	<ion-card-header>
-		<ion-card-subtitle>Great success!!</ion-card-subtitle>
-		<ion-card-title>Welcome to your app!</ion-card-title>
-	</ion-card-header>
-
-	<ion-card-content>
-		Thank you for using this starter. Click buttons below to open these guides (will
-		open in new window). Don't forget to open DevTools to see this app in mobile mode. Happy coding!!!
-	</ion-card-content>
-
-	<ion-item>
-		<ion-label>Visit Ionic Showcase app with sourceviewer</ion-label>
-		<ion-button href="https://ionicsvelte.firebaseapp.com/" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-
-	<ion-item>
-		<ion-label>Visit Ionic component docs</ion-label>
-		<ion-button
-			href="https://ionicframework.com/docs/components"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte Kit docs</ion-label>
-		<ion-button
-			href="https://kit.svelte.dev/docs/introduction"
-			target="_new"
-			fill="outline"
-			slot="end">View</ion-button
-		>
-	</ion-item>
-	<ion-item>
-		<ion-label>Visit Svelte docs</ion-label>
-		<ion-button href="https://svelte.dev/docs" target="_new" fill="outline" slot="end"
-			>View</ion-button
-		>
-	</ion-item>
-</ion-card>
-
-  
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import Landing from '$lib/components/landing/Landing.svelte';
+    import Nav from '$lib/components/Nav.svelte';
+    import { onMount } from 'svelte';
+    import { Capacitor } from '@capacitor/core';
+    const isPWA = (win: Window): boolean =>
+        !!(win.matchMedia?.('(display-mode: standalone)').matches || 
+                (win.navigator as any).standalone);
+    let showLanding = false;
+    onMount(() => {
+        console.log('Are we native?', Capacitor.isNativePlatform());
+        if (Capacitor.isNativePlatform()) {
+            console.log('Found native shell, redirecting');
+            goto('/app/login');
+            return;
+        }
+        if (isPWA(window)) {
+            console.log('In PWA - on wrong route - redirecting');
+            goto('/app/splash');
+        } else showLanding = !isPWA(window);
+    });
+</script>
+{#if showLanding}
+    <Landing />
+{/if}
