3b10a37aa6cd2d816b6625d8294ae42e74925728
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-11 01:17:20 -0700

    nav bar wrong color

--- client/src/lib/components/Nav.svelte
+++ client/src/lib/components/Nav.svelte
@@ -1,26 +1,23 @@
 <ion-tabs>
     <ion-tab-bar slot="bottom">
-        <ion-tab>
+        <ion-tab-button tab="home">
+            <ion-icon :icon="home-outline">home</ion-icon>
+        </ion-tab-button>
 
-            <ion-tab-button tab="home">
-                <ion-icon :icon="home-outline">home</ion-icon>
-            </ion-tab-button>
-
-            <ion-tab-button tab="library">
-                <ion-icon :icon="library-outline">lib</ion-icon>
-            </ion-tab-button>
+        <ion-tab-button tab="library">
+            <ion-icon :icon="library-outline">lib</ion-icon>
+        </ion-tab-button>
             
-            <ion-tab-button tab="publish">
-                <ion-icon :icon="pencil-outline">pub</ion-icon>
-            </ion-tab-button>
+        <ion-tab-button tab="publish">
+            <ion-icon :icon="pencil-outline">pub</ion-icon>
+        </ion-tab-button>
 
-            <ion-tab-button tab="search">
-                <ion-icon :icon="search-outline">search</ion-icon>
-            </ion-tab-button>
+        <ion-tab-button tab="search">
+            <ion-icon :icon="search-outline">search</ion-icon>
+        </ion-tab-button>
 
-            <ion-tab-button tab="user">
-                <ion-icon :icon="person-outline">user</ion-icon>
-            </ion-tab-button>
-        </ion-tab>
+        <ion-tab-button tab="user">
+            <ion-icon :icon="person-outline">user</ion-icon>
+        </ion-tab-button>
     </ion-tab-bar>
 </ion-tabs>
