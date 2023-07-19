d55e33ca75f50ae4bcb67494d64731aec21eaeae
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-19 01:50:40 -0700

    rough skeleton of currently reading card

---
+++ client/src/lib/components/Reading.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let coverImg:string, title:string, author:string, description:String;
+</script>
+
+<ion-card>
+    <ion-card-header>
+        <ion-card-title>Currently Reading...</ion-card-title>
+    </ion-card-header>
+
+    <ion-grid>
+        <ion-row>
+            <ion-col>
+                <ion-img src={coverImg}></ion-img>
+            </ion-col>
+            <ion-col>
+                <h1>{title}</h1>
+                <h2>{author}</h2>
+                <p>{description}</p>
+            </ion-col>
+        </ion-row>
+    </ion-grid>
+</ion-card>
