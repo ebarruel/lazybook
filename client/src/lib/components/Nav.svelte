2320a1039c147dd1dab044d74c4fe2b6c2064117
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-20 16:17:40 -0700

    abstracted icon names

--- client/src/lib/components/Nav.svelte
+++ client/src/lib/components/Nav.svelte
@@ -4,29 +4,36 @@
     import IonPencilOutline from '~icons/ion/pencil-outline';
     import IonSearchOutline from '~icons/ion/search-outline';
     import IonPersonOutline from '~icons/ion/person-outline';
+
+    // rename to allow for easy replacement
+    let HomeIcon = IonHomeOutline;
+    let LibIcon = IonLibraryOutline;
+    let PubIcon = IonPencilOutline;
+    let SearchIcon = IonSearchOutline;
+    let UserIcon = IonPersonOutline;
 </script>
 
 <ion-tab-button tab="home">
-    <IonHomeOutline></IonHomeOutline>
+    <HomeIcon></HomeIcon>
     <ion-label>home</ion-label>
 </ion-tab-button>
 
 <ion-tab-button tab="library">
-    <IonLibraryOutline></IonLibraryOutline>
+    <LibIcon></LibIcon>
     <ion-label>lib</ion-label>
 </ion-tab-button>
     
 <ion-tab-button tab="publish">
-    <IonPencilOutline></IonPencilOutline>
+    <PubIcon></PubIcon>
     <ion-label>pub</ion-label>
 </ion-tab-button>
 
 <ion-tab-button tab="search">
-    <IonSearchOutline></IonSearchOutline>
+    <SearchIcon></SearchIcon>
     <ion-label>search</ion-label>
 </ion-tab-button>
 
 <ion-tab-button tab="user">
-    <IonPersonOutline></IonPersonOutline>
+    <UserIcon></UserIcon>
     <ion-label>user</ion-label>
 </ion-tab-button>
