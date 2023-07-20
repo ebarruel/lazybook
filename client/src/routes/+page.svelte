f16ec1f9fb8c729ef0f431bbf0e9138e364a0162
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-20 09:10:24 -0700

    get rid of landing (for now)

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
-    import Landing from '$lib/components/landing/Landing.svelte';
     import { onMount } from 'svelte';
     import { Capacitor } from '@capacitor/core';
 
     const isPWA = (win: Window): boolean =>
         !!(win.matchMedia?.('(display-mode: standalone)').matches || 
                 (win.navigator as any).standalone);
-
-    let showLanding = false;
     
     onMount(() => {
         console.log('Are we native?', Capacitor.isNativePlatform());
