d0a0f809d2dada48381f6933e7088bc39e858203
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-22 00:20:06 -0700

    added some mock api

--- client/src/server.js
+++ client/src/server.js
@@ -4,13 +4,39 @@ createServer({
 	routes() {
 		this.namespace = "api";
 
-		this.get("/movies", () => {
+		this.get("/title", () => {
 			return {
-				movies: [
-					{ id: 1, name: "Inception", year: 2010 },
-					{ id: 2, name: "Interstellar", year: 2014 },
-					{ id: 3, name: "Dunkirk", year: 2017 }
-				]
+				title: "The Title"
+			};
+		});
+
+		this.get("/authors", () => {
+			return {
+				authors: ["Author 1", "Author 2"]
+			};
+		});
+
+		this.get("/contributors", () => {
+			return {
+				contributors: [ "Contributor 1", "Contributor 2" ]
+			};
+		});
+
+		this.get("/publisher", () => {
+			return {
+				publisher: [ "Publisher" ]
+			};
+		});
+
+		this.get("/cover", () => {
+			return {
+				cover: "$samples/accessible_epub_3/EPUB/covers/9781449328030_lrg.jpg"
+			};
+		});
+
+		this.get("/description", () => {
+			return {
+				description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 			};
 		});
 	}
