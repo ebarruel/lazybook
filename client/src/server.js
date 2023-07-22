e1df7631654c75558f3cb3e19e6f543037b19ec9
ebarruel <barruelevangeline.bchs@gmail.com>
2023-07-22 01:07:16 -0700

    add entire book item

--- client/src/server.js
+++ client/src/server.js
@@ -3,6 +3,19 @@ import { createServer } from "miragejs";
 createServer( {
 	routes() {
 		this.namespace = "api";
+		this.urlPrefix = 'http://localhost:5174';
+
+		this.get( "/book", () => {
+			return {
+				title: "The Title",
+				authors: [ "Author 1", "Author 2" ],
+				contributors: [ "Contributor 1", "Contributor 2" ],
+				publisher: [ "Publisher" ],
+				cover: "$samples/accessible_epub_3/EPUB/covers/9781449328030_lrg.jpg",
+				description:
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+			};
+		} );
 
 		this.get( "/title", () => {
 			return {
