c93565662bc61ea5fd98f59ee602c527935391bd
ebarruel <barruelevangeline.bchs@gmail.com>
2023-08-01 01:15:39 -0700

    remove code from failed mock additions

--- client/src/app.d.ts
+++ client/src/app.d.ts
@@ -2,12 +2,6 @@
 // for information about these interfaces
 import "unplugin-icons/types/svelte";
 
-if (process.env.NODE_ENV === 'development') {
-	import { worker } from "./mocks/browser";
-
-	worker.start();
-}
-
 declare global {
 	namespace App {
 		// interface Error {}
--- client/tests/api.test.ts
+++ client/tests/api.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from './test';
+import { test, expect } from "@playwright/test";
 
 test.beforeEach( async ({page}) => {
 	page.goto("/");
