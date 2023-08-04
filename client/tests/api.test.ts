85b02bfe22a41d9f724f7cb16af2bfc1e19a5b77
ebarruel <barruelevangeline.bchs@gmail.com>
2023-08-04 02:04:35 -0700

    format (with bun)

--- client/tests/api.test.ts
+++ client/tests/api.test.ts
@@ -4,7 +4,7 @@ test.beforeEach(async ({ page }) => {
 	page.goto("/");
 });
 
-test("get book", async ({page}) => {
+test("get book", async ({ page }) => {
 	await expect(page.getByText("Currently Reading...")).toBeVisible();
 	await expect(page.getByAltText("Currently Reading Cover Image")).toBeVisible();
 });
